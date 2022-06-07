05d758fbd27e139325c1061e62f958a2f1268f1e
pbennettibm <Patrick.Bennett@ibm.com>
2022-06-07 15:10:17 -0500

    attempt to use box-sdx w/personal

--- box-sdk.js
+++ box-sdk.js
@@ -1,12 +1,14 @@
 const BoxSDK = require('box-node-sdk');
+var sdkConfig = require('./config.json');
+require('dotenv').config();
 
 // Initialize the SDK with your app credentials
 const sdk = new BoxSDK({
-  clientID: 'CLIENT_ID',
-  clientSecret: 'CLIENT_SECRET',
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
 // Create a basic API client, which does not automatically refresh the access token
-const client = sdk.getBasicClient('DEVELOPER_TOKEN');
+const client = sdk.getBasicClient(process.env.DEV_TOKEN);
 
 module.exports = client;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Box = require('./box-sdk');
+const axios = require('axios');
+require('dotenv').config();
 const app = express();
 const port = 3001;
 
@@ -7,22 +9,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/csv', (req, res) => {
-  if (req.body.filename) {
-    
-    // transform to csv
+  if (req.body.businessName) {
+    let csv = '';
 
-    Box.files
-      .uploadFile(
-        'doodlebugs-norfolksouthern-w9-external-confidential',
-        req.body.filename,
-        fileContents
-      )
-      .then((fileObject) => {
+    for (let prop in req.body) {
+      csv += `${prop}, `;
+    }
+
+    csv += '\n';
+
+    for (let prop in req.body) {
+      csv += `${req.body[prop]}, `;
+    }
+
+    const axiosConfig = {
+      method: 'post',
+      url: 'https://upload.box.com/api/2.0/files/content',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + process.env.DEV_TOKEN,
+      },
+      data: JSON.stringify(csv),
+    };
+
+    axios(axiosConfig)
+      .then(function (response) {
+        console.log(response);
         res.status(201).end();
       })
-      .catch((error) => {
+      .catch(function (error) {
+        console.log(error);
         res.status(400).end();
       });
+
+  //   Box.files
+  //     .uploadFile('My Box Notes', `${req.body.businessName}.csv`, csv)
+  //     .then((fileObject) => {
+  //       res.status(201).end();
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       res.status(400).end();
+  //     });
   } else {
     res.status(406).end();
   }
