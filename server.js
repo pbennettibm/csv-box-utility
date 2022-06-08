0e04d547b723d99b11f59852d77cc92972e1779b
pbennettibm <Patrick.Bennett@ibm.com>
2022-06-08 12:29:54 -0500

    display filenames

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 const express = require('express');
 require('dotenv').config();
 const fs = require('fs');
+const fsPromise = ('fs-promise');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+const directoryPath = path.join(__dirname, 'downloads');
+// app.use(express.static(directoryPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const directoryPath = path.join(__dirname, 'downloads');
 
 app.post('/csv', (req, res) => {
   if (req.body.businessName) {
@@ -33,44 +33,33 @@ app.post('/csv', (req, res) => {
     fs.writeFileSync(`${directoryPath}/${req.body.businessName}.csv`, csv);
     console.log(`${directoryPath}/${req.body.businessName}.csv created`);
     res.status(201).end();
-    // const axiosConfig = {
-    //   method: 'post',
-    //   url: 'https://upload.box.com/api/2.0/files/content',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: 'Bearer ' + process.env.DEV_TOKEN,
-    //   },
-    //   data: JSON.stringify(csv),
-    // };
-
-    // axios(axiosConfig)
-    //   .then(function (response) {
-    //     console.log(response);
-    //     res.status(201).end();
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //     res.status(400).end();
-    //   });
-
-  //   Box.files
-  //     .uploadFile('My Box Notes', `${req.body.businessName}.csv`, csv)
-  //     .then((fileObject) => {
-  //       res.status(201).end();
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //       res.status(400).end();
-  //     });
   } else {
     res.status(406).end();
   }
 });
 
-// app.get('/download', (req, res) => {
-//   res.download(`${directoryPath}/business1.csv`);
-//   // res.status(200).end();
-// });
+app.get('/', async (req, res) => {
+  let html = '';
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).end();
+    } else {
+      files.forEach((file) => {
+        html += `<a href="/download:${file}">${file}</a>
+          <br>
+          <br>`;
+      });
+      res.send(html);
+      res.status(200).end();
+    }
+  });
+});
+
+app.get('/download', (req, res) => {
+  console.log(req.params)
+  res.download(filePath);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
